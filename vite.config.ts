9fd13dc7cb4ed057e6a3c6e49430d82b2aa4543e
Andri Anto <andrianto.ykhc@gmail.com>
2023-04-14 00:33:12 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/resume",
+
     resolve: {
         alias: {
             "@": resolve(__dirname, "./src"),
